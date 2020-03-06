1fa9e39d2a762dc3533df802983ecd5946575f16
AppliedCryptoLab <scorpioskpo@gmail.com>
2020-03-06 11:29:56 +0800

    init the package structure

---
+++ salrs/salrs.go
@@ -0,0 +1,41 @@
+package salrs
+
+/*
+This file contains all the public constant, type, and functions that are available to oue of the package.
+*/
+
+//	public const def	begi
+//	const MpkSize =1000
+//	public const def	end=1000
+
+//	public type def		begin
+type MasterPubKey struct {
+}
+
+type MasterViewSecretKey struct {
+}
+
+type MasterSignSecretKey struct {
+}
+
+type DerivedPubKey struct {
+}
+
+type DpkRing struct {
+}
+
+type Signature struct {
+}
+
+type KeyImage struct {
+}
+
+//	public type def		end
+
+//	public fun	begin
+
+//	public fun	end
+
+//	private field (optional)	begin
+
+//	private field (optional)	end
